ba8112d6d615f4d5d09c6e8225b3f04a6a5acf55
Henry-wxq <henry.wxq.1118@gmail.com>
2023-09-26 18:42:34 -0400

    Updated JavaSE

---
+++ Java/JavaSE/day02-basic-app/src/com/henryw/operator/OperatorDemo4.java
@@ -0,0 +1,20 @@
+package com.henryw.operator;
+
+/**
+ * 关系运算符，与Python中一样，返回true或者false
+ */
+public class OperatorDemo4 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        // >
+        System.out.println(a > b);
+
+        // <
+        // >=
+        // <=
+        // ==
+        // !=
+    }
+}
