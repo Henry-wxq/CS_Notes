d2a50b5d6c7d1552adc27a32fbee4a62159dba7a
Henry-wxq <henry.wxq.1118@gmail.com>
2023-09-26 01:35:40 -0400

    Updated JavaSE

--- Java/JavaSE/day02-basic-app/src/com/henryw/type/TypeConversionDemo1.java
+++ Java/JavaSE/day02-basic-app/src/com/henryw/type/TypeConversionDemo1.java
@@ -2,6 +2,8 @@ package com.henryw.type;
 
 /**
  * 自动类型转换：类型范围小的变量，可以直接赋值给类型范围大的变量
+ * byte -> short -> int -> long -> float -> double
+ * char -> int -> long -> float -> double
  */
 
 public class TypeConversionDemo1 {
@@ -10,5 +12,11 @@ public class TypeConversionDemo1 {
         int b = a; // 发生了自动类型转换
         System.out.println(a);
         System.out.println(b);
+
+        // char -> float
+        // 'a' 97 => 00000000 01100001
+        char c = 'a';
+        float d = c;
+        System.out.println(d);
     }
 }
