a0c1fbcd9898211a9d32ae6efd3a402f77488537
Henry-wxq <henry.wxq.1118@gmail.com>
2023-09-27 01:04:13 -0400

    Updated JavaSE

--- Java/JavaSE/day03-flow-control-app/src/com/henryw/loop/ForDemo2.java
+++ Java/JavaSE/day03-flow-control-app/src/com/henryw/loop/ForDemo2.java
@@ -1,9 +1,31 @@
 package com.henryw.loop;
 
-// 批量产生数据
+// for loop 批量产生数据
 
 public class ForDemo2 {
     public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+
+        System.out.println("-----------------------------------------------");
+
+        // 1到100奇数的和
+        // 方法一
+        int sum_1 = 0;
+        for (int i = 1; i <= 100; i+=2) {
+            sum_1 += i;
+        }
+        System.out.println(sum_1);
+
+        // 方法二
+        int sum_2 = 0;
+        for (int i = 0; i <= 100; i++) {
+            if (i % 2 == 1) {
+                sum_2 += i;
+            }
+        }
+        System.out.println(sum_2);
 
     }
 }
