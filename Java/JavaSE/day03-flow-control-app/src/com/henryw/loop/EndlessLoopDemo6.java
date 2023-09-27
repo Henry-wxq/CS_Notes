fad9a87f0b85074fc525650ae86053b4b03569cd
Henry-wxq <henry.wxq.1118@gmail.com>
2023-09-27 01:34:08 -0400

    Updated JavaSE

---
+++ Java/JavaSE/day03-flow-control-app/src/com/henryw/loop/EndlessLoopDemo6.java
@@ -0,0 +1,24 @@
+package com.henryw.loop;
+
+/**
+ * 死循环的应用场景：服务器程序(让服务器不断执行，可以随时接收请求)
+ */
+
+public class EndlessLoopDemo6 {
+    public static void main(String[] args) {
+        // for looop
+//        for ( ; ; ) {
+//            System.out.println("Hello World");
+//        }
+
+        // 经典写法，因为不知道要执行多少次
+//        while (true) {
+//            System.out.println("Hello World");
+//        }
+
+        // do while
+//        do {
+//            System.out.println("Hello World");
+//        } while (true);
+    }
+}
