d36ebc1c8a3d4c6e55b92187ad9259cf3e6fceaa
Henry-wxq <henry.wxq.1118@gmail.com>
2023-09-27 01:07:39 -0400

    Updated JavaSE

---
+++ Java/JavaSE/day03-flow-control-app/src/com/henryw/loop/WhileDemo3.java
@@ -0,0 +1,13 @@
+package com.henryw.loop;
+
+/**
+ * 1. while循环的书写格式：
+ *      初始化语句;
+ *      while (循环条件) {
+ *          循环体语句(被重复执行的代码);
+ *          迭代语句;
+ *      }
+ */
+
+public class WhileDemo3 {
+}
