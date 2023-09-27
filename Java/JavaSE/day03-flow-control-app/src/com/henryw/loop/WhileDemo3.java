f60af3c94cfde08c2fffc0d662c07ac2d6244c09
Henry-wxq <henry.wxq.1118@gmail.com>
2023-09-27 01:10:46 -0400

    Updated JavaSE

--- Java/JavaSE/day03-flow-control-app/src/com/henryw/loop/WhileDemo3.java
+++ Java/JavaSE/day03-flow-control-app/src/com/henryw/loop/WhileDemo3.java
@@ -7,7 +7,18 @@ package com.henryw.loop;
  *          循环体语句(被重复执行的代码);
  *          迭代语句;
  *      }
+ *
+ * 2. 知道循环几次用for，不知道循环几次建议使用while
  */
 
 public class WhileDemo3 {
+    public static void main(String[] args) {
+        // 打印多行Hello World
+        int i = 0;
+        while (i <= 5) {
+            System.out.println("Hello World");
+            i ++;
+        }
+
+    }
 }
