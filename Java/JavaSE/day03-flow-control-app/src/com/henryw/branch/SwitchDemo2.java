5bc41eb21d556872dc4b9f1ad6cf3e56ff6c3047
Henry-wxq <henry.wxq.1118@gmail.com>
2023-09-27 00:21:31 -0400

    Updated JavaSE

---
+++ Java/JavaSE/day03-flow-control-app/src/com/henryw/branch/SwitchDemo2.java
@@ -0,0 +1,37 @@
+package com.henryw.branch;
+
+/**
+ * switch分支通过比较值来决定执行哪条分支
+ * 执行流程：
+ * 1. 先执行表达式的值，再拿着这个值去与case后的值进行匹配
+ * 2. 与哪个case后的值匹配为true就执行哪个case块的代码，遇到break就跳出switch分支
+ * 3. 如果全部case后的值与之相匹配都是false，则执行default块的代码
+ *
+ * 当条件是一个一个值比较的时候，switch分支更合适：格式良好，性能较好
+ * 当条件是区间的时候，应该用if分支
+ */
+
+public class SwitchDemo2 {
+    public static void main(String[] args) {
+        // 每天做不一样的事情
+        String week = "周三";
+        switch (week) {
+            case "周一":
+                System.out.println("埋头苦干，解决bug");
+            case "周二":
+                System.out.println("请教他人");
+            case "周三":
+                System.out.println("龙虾烧烤");
+            case "周四":
+                System.out.println("今晚吃鸡");
+            case "周五":
+                System.out.println("帮助女程序员解决问题");
+            case "周六":
+                System.out.println("酒吧嗨皮");
+            case "周日":
+                System.out.println("郁郁寡欢，准备上班");
+            default:
+                System.out.println("星期不存在");
+        }
+    }
+}
