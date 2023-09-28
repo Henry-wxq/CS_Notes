58767f22d49d830e7993f2ec0b13f6146e54ec36
Henry-wxq <henry.wxq.1118@gmail.com>
2023-09-28 18:29:14 -0400

    Updated JavaSE

--- Java/JavaSE/day04-array-app/src/com/henryw/demo/Test1.java
+++ Java/JavaSE/day04-array-app/src/com/henryw/demo/Test1.java
@@ -5,4 +5,26 @@ package com.henryw.demo;
  */
 
 public class Test1 {
+    public static void main(String[] args) {
+        // 1. 准备一个数组
+        int[] arr = {10, 20, 30, 40, 50};
+
+        // 2. 定义一个循环，这集两个变量，一个在前一个灾后
+        // 注意此时","的使用
+        for (int i = 0, j = arr.length - 1; i < j; i++, j--) {
+            // arr[i] arr[j]
+            // 1. 创建一个临时变量，储存最后的值
+            int temp = arr[j];
+
+            // 2. 将前面的值赋给后面
+            arr[j] = arr[i];
+
+            // 3. 将临时变量的值赋给前面
+            arr[i] = temp;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
 }
