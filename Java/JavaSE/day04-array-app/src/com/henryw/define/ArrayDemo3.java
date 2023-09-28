bf9c15367685d7d4b7f7c9a802b35f5c8f21f589
Henry-wxq <henry.wxq.1118@gmail.com>
2023-09-28 01:24:34 -0400

    Updated JavaSE

---
+++ Java/JavaSE/day04-array-app/src/com/henryw/define/ArrayDemo3.java
@@ -0,0 +1,17 @@
+package com.henryw.define;
+
+/**
+ * 遍历数组：求和，元素搜索，找最大值，最小值
+ */
+
+public class ArrayDemo3 {
+    public static void main(String[] args) {
+        int[] age_array = {12, 24, 36};
+
+        // 快速书写数组遍历：直接敲：age_array.fori 即可
+        for (int i = 0; i < age_array.length; i++) {
+            System.out.println(age_array[i]);
+        }
+    }
+
+}
