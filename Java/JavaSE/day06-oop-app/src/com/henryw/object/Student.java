8de383fa3459032eb0d18ced0e0cd38a518ea7ad
Henry-wxq <henry.wxq.1118@gmail.com>
2023-09-29 15:23:00 -0400

    Updated JavaSE

--- Java/JavaSE/day06-oop-app/src/com/henryw/object/Student.java
+++ Java/JavaSE/day06-oop-app/src/com/henryw/object/Student.java
@@ -1,20 +1,23 @@
 package com.henryw.object;
 
 /**
- *
+ * 面向对象编程：先创建一个类结构，开发一个个对象，把数据交给对象，再调用对象的方法来完成对数据的处理
+ * Test.java
  */
 
 public class Student {
     // 打印学生总成绩，平均成绩
-    String name;
-    double chinese;
-    double math;
+    // 先定义变量
+    String name; // 姓名
+    double chinese; // 语文成绩
+    double math; // 数学成绩
 
+    // 然后定义方法
     public void printTotalScore() {
-        System.out.println(name + "的总成绩是：" + (chinese + math));
+        System.out.println(name + "的总成绩是: " + (chinese + math));
     }
 
     public void printAverageScore() {
-        System.out.println(name + "的平均成绩是" + (chinese + math) / 2);
+        System.out.println(name + "的平均成绩是: " + (chinese + math) / 2);
     }
 }
---
+++ Java/JavaSE/day06-oop-app/src/com/henryw/object/Test.java
@@ -0,0 +1,30 @@
+package com.henryw.object;
+
+/**
+ * 右键Student.java放到右边窗口
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        // 1. 创建一个学生对象，封装波妞数据
+        Student s1 = new Student();
+        s1.name = "波妞";
+        s1.chinese = 100;
+        s1.math = 100;
+
+        // 调用方法
+        // 因为是通过s1调用的，只会使用s1里面的数据
+        s1.printTotalScore();
+        s1.printAverageScore();
+
+        // 2. 再创建一个学生对象，封装波仔数据
+        Student s2 = new Student();
+        s2.name = "波仔";
+        s2.chinese = 59;
+        s2.math = 100;
+
+        s2.printTotalScore();
+        s2.printAverageScore();
+
+    }
+}
