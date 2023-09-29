a828a214cdb8b3a2acd78805c61ef8199547252f
Henry-wxq <henry.wxq.1118@gmail.com>
2023-09-29 01:44:33 -0400

    Updated JavaSE

---
+++ Java/JavaSE/day05-method-app/src/com/henryw/overload/MethodOverloadDemo1.java
@@ -0,0 +1,22 @@
+package com.henryw.overload;
+
+/**
+ * 方法重载：一个类中，出现多个方法的名称相同，但是它们的形参列表是不同的，那么这些方法就成为方法重载了，不会起冲突
+ *
+ * 一个类中，只要名称一样，形参列表不同，那么就是方法重载了，如修饰符，返回值类型是否一样都无所谓
+ * 形参列表不同，指的是：形参的个数，类型，顺序不同，不关心形参的名称
+ */
+public class MethodOverloadDemo1 {
+    public static void main(String[] args) {
+        test();
+        test(100);
+    }
+
+    public static void test() {
+        System.out.println("===test1===");
+    }
+
+    public static void test(int a) {
+        System.out.println("===test2===" + a);
+    }
+}
---
+++ Java/JavaSE/day05-method-app/src/com/henryw/overload/MethodOverloadDemo2.java
@@ -0,0 +1,9 @@
+package com.henryw.overload;
+
+/**
+ *  方法重载的应用场景
+ *  开发中我们经常需要为处理一类业务，提供多种解决方案，此时方法重载来设计是很专业的
+ */
+
+public class MethodOverloadDemo2 {
+}
