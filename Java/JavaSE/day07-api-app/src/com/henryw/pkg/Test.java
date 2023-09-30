b2587cabb51b97103b82e0e42407a977d7e348a8
Henry-wxq <henry.wxq.1118@gmail.com>
2023-09-30 16:12:44 -0400

    Updated JavaSE

--- Java/JavaSE/day07-api-app/src/com/henryw/pkg/Test.java
+++ Java/JavaSE/day07-api-app/src/com/henryw/pkg/Test.java
@@ -5,7 +5,7 @@ package com.henryw.pkg;
  * 2. 访问其它包下的程序，必须导包才可以访问
  * 自动导包：settings - auto import - add unambiguous imports on the fly
  * 3. 自己的程序中调用Java提供的程序，也需要先导包才可以使用；注意，Java.lang包下的程序时不需要我们导包的，可以直接使用 e.g. String
- * 4. 访问多个包下的城区，这些程序名又一样的情况下，默认只能导入一个程序，另一个程序必须带包名和类名来访问
+ * 4. 访问多个包下的程序，这些程序名又一样的情况下，默认只能导入一个程序，另一个程序必须带包名和类名来访问
  */
 
 public class Test {
