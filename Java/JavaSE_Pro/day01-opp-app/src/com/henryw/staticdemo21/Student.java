dd726210064204585b159bd58af4b2207bf81787
Henry-wxq <henry.wxq.1118@gmail.com>
2023-10-04 00:13:32 -0400

    Updated JavaSE_Pro

--- Java/JavaSE_Pro/day01-opp-app/src/com/henryw/staticdemo21/Student.java
+++ Java/JavaSE_Pro/day01-opp-app/src/com/henryw/staticdemo21/Student.java
@@ -1,4 +1,17 @@
 package com.henryw.staticdemo21;
 
 public class Student {
+
+    double score;
+
+    // 类方法
+    public static void printHelloWorld(){
+        System.out.println("Hello World 1");
+        System.out.println("Hello World 2");
+    }
+
+    // 实例方法
+    public void printPass(){
+        System.out.println("成绩" + (score >= 60 ? "及格" : "不及格"));
+    }
 }
--- Java/JavaSE_Pro/day01-opp-app/src/com/henryw/staticdemo21/Test.java
+++ Java/JavaSE_Pro/day01-opp-app/src/com/henryw/staticdemo21/Test.java
@@ -1,4 +1,13 @@
 package com.henryw.staticdemo21;
 
 public class Test {
+    public static void main(String[] args) {
+        // 1. 类方法的用法
+        // 类名.类方法(推荐) / 对象名.类方法(不推荐)
+        Student.printHelloWorld();
+
+        Student s = new Student();
+        s.score = 61;
+        s.printPass();
+    }
 }
