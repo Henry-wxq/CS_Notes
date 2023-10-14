b326b657ae645471168ab748e77b4b0e413bc296
Henry-wxq <henry.wxq.1118@gmail.com>
2023-10-14 18:50:12 -0400

    Updated JavaSE_Pro

--- Java/JavaSE_Pro/day01-opp-app/src/com/henryw/staticdemo21/Test.java
+++ Java/JavaSE_Pro/day01-opp-app/src/com/henryw/staticdemo21/Test.java
@@ -1,6 +1,8 @@
 package com.henryw.staticdemo21;
 
 public class Test {
+    // main方法是个类方法
+    // 通过虚拟机调类名完成Test.main()来执行的
     public static void main(String[] args) {
         // 1. 类方法的用法
         // 类名.类方法(推荐) / 对象名.类方法(不推荐)
