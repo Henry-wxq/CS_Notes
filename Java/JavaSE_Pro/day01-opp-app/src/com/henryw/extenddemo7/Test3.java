c64ad0744c505839ab8aaad5dcc5601e4ecf0572
Henry-wxq <henry.wxq.1118@gmail.com>
2023-10-15 16:50:53 -0400

    Updated JavaSE_Pro

---
+++ Java/JavaSE_Pro/day01-opp-app/src/com/henryw/extenddemo7/Test3.java
@@ -0,0 +1,61 @@
+package com.henryw.extenddemo7;
+
+/**
+ * 通过this(...)调用兄弟构造器
+ */
+
+public class Test3 {
+    public static void main(String[] args) {
+        Student s1 = new Student("Henry", 20, "UOT");
+
+        // 需求：如果学生没有填写学校，那么学校默认就是SCIE
+        // 可以提供一个额外有参构造器
+        Student s2 = new Student("Michelle", 20);
+        System.out.println(s2.getName());
+        System.out.println(s2.getAge());
+        System.out.println(s2.getSchoolName());
+    }
+}
+
+class Student{
+    private String name;
+    private int age;
+    private String schoolName;
+
+    public Student() {
+    }
+
+    public Student(String name, int age){
+        this(name, age, "SCIE"); // 调用兄弟构造器
+    }
+
+    public Student(String name, int age, String schoolName) {
+        this.name = name;
+        this.age = age;
+        this.schoolName = schoolName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSchoolName() {
+        return schoolName;
+    }
+
+    public void setSchoolName(String schoolName) {
+        this.schoolName = schoolName;
+    }
+}
