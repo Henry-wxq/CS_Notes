abd64099978dff04e22088422b61de0bc0972c21
Henry-wxq <henry.wxq.1118@gmail.com>
2023-11-08 17:14:30 -0500

    Updated JavaSE_Pro

--- Java/JavaSE_Pro/day03-oop-app3/src/com/henryw/genericdemo7/Test1.java
+++ Java/JavaSE_Pro/day03-oop-app3/src/com/henryw/genericdemo7/Test1.java
@@ -1,7 +1,15 @@
 package com.henryw.genericdemo7;
-
-import java.lang.reflect.Array;
 import java.util.ArrayList;
+
+/**
+ * 泛型：定义类，接口，方法时，同时声明了一个或者多个类型变量(如：<E>)，称为泛型类，泛型接口，泛型方法，它们统称为泛型
+ * e.g. public class ArrayList<E>{
+ *
+ *      }
+ *
+ * 作用：泛型提供了在编译阶段约束所能操作的数据类型，并自动进行检查的能力；可以避免强制类型转换，及其可能出现的异常
+ * 本质：把具体的数据类型作为参数传给类型变量
+ */
 
 public class Test1 {
     public static void main(String[] args) {
@@ -17,6 +25,17 @@ public class Test1 {
             String e = (String) list.get(i);
             System.out.println(e); // 猫对象报错
 
+        }
+
+        System.out.println("-------------------------------------");
+        // 使其只能加入String，约束能加入的类型
+        ArrayList<String> list1 = new ArrayList<>(); // 从JDK1.7开始后面的尖括号中可以不用声明
+        list1.add("Java1");
+        // list.add(new Cat()); // 报错
+
+        for (int i = 0; i < list.size(); i++) {
+            String e = list1.get(i);
+            System.out.println(e);
         }
     }
 }
